d372bfd699b2ead769a212b7788c7439f7bd6514
disaq1 <slava79198015981@yandex.ru>
2021-02-27 16:57:57 +0400

    написал скрипт для модального окна (open&close)

--- #src/js/script.js
+++ #src/js/script.js
@@ -1,9 +1,47 @@
+
+
+//Функция для смены класса кнопки по нажатию
+
 function btnToggle(inCart) {
   inCart.classList.toggle('inCart');
   if (inCart.classList.contains('inCart')) {
-      inCart.innerText = 'В корзине'
+      inCart.innerText = 'В корзине';
   } else {
       inCart.innerText = 'Добавить в корзину'
   }
 }
 
+//Функция, объединяющая тоггл и добавление класса
+
+// document.querySelector('.products__button').onclick = function(event) {
+//   btnToggle(inCart);
+//   document.querySelector('.cart_num').classList.add('cart_num-on');
+// }
+
+//Скрипт для открытия модального окна
+
+document.getElementById('open-popup').onclick = function(event) {
+  document.querySelector('.modal-wrap').classList.add('popup');
+  document.querySelector('.modal').classList.add('popup');
+}
+
+//Скрипт для закрытия модального окна
+
+document.getElementById('close-popup').onclick = function(event) {
+  document.querySelector('.modal-wrap').classList.remove('popup');
+  document.querySelector('.modal').classList.remove('popup');
+}
+
+//Скрипт для блокировки кнопки отправки формы
+
+
+const form = document.querySelector('form');
+const formSubmit = document.querySelector('.form-button');
+
+function changeFormHandler() {
+  if (form.checkValidity()) {
+    formSubmit.removeAttribute('disabled');
+    document.querySelector('.form-button').classList.add('form-btn');
+  }
+}
+form.addEventListener('change', changeFormHandler);
